943e7cb6d3e58085ff5679d6912e784e3e9811a8
Kyle Aikman <kaaikman@gmail.com>
2017-01-15 01:38:36 -0500

    Changes add/edit todo to work with object todos

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # practice-todo
+
+Testing and improving basic JS knowledge.
+Using 'Practical JavaScript' course through viewing the requirements, implementing alone, then comparing solutions.
--- todoscript.js
+++ todoscript.js
@@ -6,12 +6,18 @@ var todoList = {
     console.log('Todos: ', this.todos);
   },
   addTodo: function(newTodo) {
-    this.todos.push(newTodo);
+    this.todos.push({
+      todoText: newTodo,
+      todoIsComplete: false
+    });
   },
   removeTodo: function(todoIndex) {
     this.todos.splice(todoIndex, 1);
   },
   editTodo: function(todoIndex, todoEdit) {
-    this.todos[todoIndex] = todoEdit;
+    this.todos[todoIndex].todoText = todoEdit;
+  },
+  toggleTodoIsComplete: function(todoIndex) {
+    this.todos[todoIndex].todoIsComplete = !this.todos[todoIndex].todoIsComplete;
   }
 };
