299725b618c965f7a96bada56ad21f6aeb873734
Kyle Aikman <kaaikman@gmail.com>
2017-01-23 00:08:29 -0500

    Creates html page and moves functionality of displaying/adding from console to page

--- todoscript.js
+++ todoscript.js
@@ -3,18 +3,24 @@
 var todoList = {
   todos: [],
   displayTodos: function() {
-    if(this.todos.length === 0) {
-      console.log('Your Todo List is empty!')
+    this.listOfTodos = document.getElementById('listOfTodos');
+    this.listOfTodos.innerHTML = '';
+    if (this.todos.length === 0) {
+      // return 'Your Todo List is empty!'
+      document.getElementById('todoList').innerHTML = 'Your Todo List is empty!';
     }
     else {
-      for(i=0; i<this.todos.length; i++) {
-        console.log(this.todos[i].todoText);
-        if(this.todos[i].todoIsComplete === false) {
-          console.log('Incomplete');
+      for (i=0; i<this.todos.length; i++) {
+        // document.getElementById('todoList').innerHTML = this.todos[i].todoText;
+        var addingTodo = document.createElement('li');
+        // addingTodo.textContent = this.todos[i].todoText;
+        if (this.todos[i].todoIsComplete === false) {
+          addingTodo.textContent = '(Incomplete) ' + this.todos[i].todoText;
         }
-        else if(this.todos[i].todoIsComplete === true) {
-          console.log('Complete');
+        else if (this.todos[i].todoIsComplete === true) {
+          addingTodo.textContent = '(Complete) ' + this.todos[i].todoText;
         }
+        this.listOfTodos.appendChild(addingTodo);
       }
     }
   },
@@ -32,5 +38,41 @@ var todoList = {
   },
   toggleTodoIsComplete: function(todoIndex) {
     this.todos[todoIndex].todoIsComplete = !this.todos[todoIndex].todoIsComplete;
+  },
+  toggleAllTodoIsComplete: function() {
+    var isCompleteCount = 0;
+    for (i=0; i<this.todos.length; i++) {
+      if (this.todos[i].todoIsComplete === true) {
+        isCompleteCount++;
+      }
+    }
+    // or just an if else
+    switch (isCompleteCount === this.todos.length) {
+      // if all are true, set all false
+      case true:
+        for (i=0; i<this.todos.length; i++) {
+          this.todos[i].todoIsComplete = false;
+        }
+        break;
+      // if 1 or more is false, set all true
+      case false:
+        for (i=0; i<this.todos.length; i++) {
+          this.todos[i].todoIsComplete = true;
+        }
+        break;
+    }
+    todoList.displayTodos();
   }
 };
+
+// button functions
+
+// var btnDisplayTodos = document.getElementById('btnDisplayTodos').addEventListener('click', function() {todoList.displayTodos()});
+var btnToggleAllTodoIsComplete = document.getElementById('btnToggleAllTodoIsComplete').addEventListener('click', function() {todoList.toggleAllTodoIsComplete()});
+
+var btnAddTodo = document.getElementById('btnAddTodo').addEventListener('click', function() {
+  var todoToAdd = document.getElementById('inputAddTodo');
+  todoList.addTodo(todoToAdd.value);
+  todoToAdd.value = '';
+  todoList.displayTodos();
+});
